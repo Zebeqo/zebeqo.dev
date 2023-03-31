f5f661803dd8482f2805fa459e278e2bf90e7b66
Zebeqo <z715596546@qq.com>
2023-03-31 14:34:26 +0800

    refactor: wrap tagCountSorted to a getter

--- app/posts/CategorySection.tsx
+++ app/posts/CategorySection.tsx
@@ -1,19 +1,21 @@
 import { Badge } from "@/ui/primitive/Badge";
 import Link from "next/link";
-import { tagCountSorted } from "@/lib/posts";
+import { getTagCountSorted } from "@/lib/posts";
 
 export function CategorySection() {
   return (
     <section>
       <div className="font-serif text-3xl font-bold text-primary-11">分类</div>
       <div className="mt-6 flex h-fit flex-wrap items-start gap-2">
-        {tagCountSorted.slice(0, 6).map((tag) => {
-          return (
-            <Link key={tag[0]} href={`/posts/tags/${tag[0]}`}>
-              <Badge variant={{ color: "accent" }}>{tag[0]}</Badge>
-            </Link>
-          );
-        })}
+        {getTagCountSorted()
+          .slice(0, 6)
+          .map((tag) => {
+            return (
+              <Link key={tag[0]} href={`/posts/tags/${tag[0]}`}>
+                <Badge variant={{ color: "accent" }}>{tag[0]}</Badge>
+              </Link>
+            );
+          })}
       </div>
     </section>
   );
