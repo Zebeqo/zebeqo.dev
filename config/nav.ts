68bb801d543b4eaf2c790b22b525cb7350d30035
Zebeqo <z715596546@qq.com>
2023-04-01 17:18:02 +0800

    refactor: extract navbarItem config

---
+++ config/nav.ts
@@ -0,0 +1,18 @@
+export const nav = [
+  {
+    name: "主页",
+    value: "home",
+  },
+  {
+    name: "文章",
+    value: "posts",
+  },
+  {
+    name: "项目",
+    value: "projects",
+  },
+  {
+    name: "关于",
+    value: "about",
+  },
+];
