ea99b2d205690e0651dedc01655367641e3e5380
Zebeqo <z715596546@qq.com>
2023-04-03 01:07:44 +0800

    style(LoadingDots): update

--- components/LoadingDots.tsx
+++ components/LoadingDots.tsx
@@ -1,7 +1,7 @@
 // https://github.com/delbaoliveira/website/blob/main/ui/LoadingDots.tsx
 export const LoadingDots = () => {
   return (
-    <span>
+    <span className="text-xs text-neutral-11">
       <span className="inline-flex animate-[loading_1.4s_ease-in-out_infinite] rounded-full">
         &bull;
       </span>
