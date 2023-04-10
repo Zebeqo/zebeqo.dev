81a8cfd1f2e8a815138236e20bb54bb35adfb7cd
Zebeqo <z715596546@qq.com>
2023-04-10 21:14:07 +0800

    refactor: BadgeLink

--- app/posts/CategorySection.tsx
+++ app/posts/CategorySection.tsx
@@ -1,5 +1,4 @@
-import { Badge } from "@/ui/components/Badge";
-import Link from "next/link";
+import { BadgeLink } from "@/ui/components/Badge";
 import { getTagCountSorted } from "@/lib/posts";
 
 export function CategorySection({ className }: { className?: string }) {
@@ -9,13 +8,15 @@ export function CategorySection({ className }: { className?: string }) {
       <div className="mt-6 flex h-fit flex-wrap items-start gap-2">
         {getTagCountSorted()
           .slice(0, 6)
-          .map((tag) => {
-            return (
-              <Link key={tag[0]} href={`/posts/tags/${tag[0]}`}>
-                <Badge color="accent">{tag[0]}</Badge>
-              </Link>
-            );
-          })}
+          .map((tag) => (
+            <BadgeLink
+              key={tag[0]}
+              href={`/posts/tags/${tag[0]}`}
+              color="accent"
+            >
+              {tag[0]}
+            </BadgeLink>
+          ))}
       </div>
     </section>
   );
