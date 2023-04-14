e51839cb35c1d08417796330d89c33e0f3574bef
Zebeqo <z715596546@qq.com>
2023-04-14 12:00:27 +0800

    feat: MdxImage description

--- components/Mdx/MdxImage.tsx
+++ components/Mdx/MdxImage.tsx
@@ -4,10 +4,17 @@ import Image from "next/image";
 
 const MdxImage = forwardRef<
   React.ElementRef<typeof Image>,
-  React.ComponentPropsWithoutRef<typeof Image>
+  React.ComponentPropsWithoutRef<typeof Image> & {
+    description?: React.ReactNode;
+  }
 >(({ className, ...props }, ref) => (
-  <div className="flex items-center justify-center">
+  <div className="flex flex-col items-center">
     <Image ref={ref} className={cn("rounded-md", className)} {...props} />
+    {props.description && (
+      <p className="mt-2 text-sm text-neutral-11 md:text-base">
+        {props.description}
+      </p>
+    )}
   </div>
 ));
 MdxImage.displayName = "MdxImage";
