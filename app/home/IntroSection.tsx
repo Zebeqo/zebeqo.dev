662403f89cd7fa0df692fc3040b6b3f2dcd6dfc7
Zebeqo <z715596546@qq.com>
2023-04-19 04:08:11 +0800

    chore: word

--- app/home/IntroSection.tsx
+++ app/home/IntroSection.tsx
@@ -10,7 +10,7 @@ export const IntroSection = ({ className }: { className?: string }) => (
         欢迎来到我的个人网站。我是一个前端工程师，主要使用 React, Next.js,
         Typescript 开发网页应用。
       </p>
-      <p>我对 web 开发和视觉设计有着浓厚的兴趣，喜欢探索新的技术和工具。</p>
+      <p>我对 web 开发和设计有着浓厚的兴趣，喜欢探索新的技术和工具。</p>
       <p>我目前对工作开放，如果想要合作或交流，请随时联系我。</p>
     </div>
     <div className="mt-4 flex space-x-4">
