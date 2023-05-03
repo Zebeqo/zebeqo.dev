7bcfbdd4d1c477c4c6d8821da7ec527e67678eb7
Zebeqo <z715596546@qq.com>
2023-05-03 22:49:39 +0800

    feat: remove job-seeking info

--- app/home/IntroSection.tsx
+++ app/home/IntroSection.tsx
@@ -6,12 +6,9 @@ export const IntroSection = ({ className }: { className?: string }) => (
   <section className={cn("pt-[72px]", className)}>
     <h1 className="mb-6 font-serif text-5xl font-bold">你好, 我是 Zebeqo 👋</h1>
     <div className="leading-7">
-      <p>
-        欢迎来到我的个人网站。我是一个前端工程师，主要使用 React, Next.js,
-        Typescript 开发网页应用。
-      </p>
-      <p>我对 web 开发和设计有着浓厚的兴趣，喜欢探索新的技术和工具。</p>
-      <p>我目前对工作开放，如果想要合作或交流，请随时联系我。</p>
+      <p>欢迎来到我的个人网站。</p>
+      <p>我使用 Next.js, Typescript, TailwindCSS 开发网页应用。</p>
+      <p>我对 web 开发和设计有着浓厚的兴趣，喜欢探索和使用新的工具。</p>
     </div>
     <div className="mt-4 flex space-x-4">
       {sns.map((item) => {
